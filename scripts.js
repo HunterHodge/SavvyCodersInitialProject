140b4057bb2928c8ac18446b198cff52843d70eb
Hunter Hodge <hunterdhodge@gmail.com>
2019-07-09 21:01:25 -0500

    3. Construct or concatonate a string with 'hello' and the value stored in memory for their name.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 // Create a greeter that will use the person's name and display, "Hello, [Name]" in, <h1>.
 /*
-
-  3. Construct or concatonate a string with 'hello' and the value stored in memory for their name.
   4. Select the <h1> on the page.
   5. Replace the value of the <h1> tag.
 */
 
 const name = prompt('What is your name?');
+const greeting = `Hello ${name}`;
