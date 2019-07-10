8236beddec2810137cca82751d085387c24e6026
Hunter Hodge <hunterdhodge@gmail.com>
2019-07-09 21:06:15 -0500

    4. Select the <h1> on the page. 5. Replace the value of the <h1> tag.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
 // Create a greeter that will use the person's name and display, "Hello, [Name]" in, <h1>.
-/*
-  4. Select the <h1> on the page.
-  5. Replace the value of the <h1> tag.
-*/
-
 const name = prompt('What is your name?');
-const greeting = `Hello ${name}`;
+
+document.querySelector('h1').textContent = `Hello ${name}`;
