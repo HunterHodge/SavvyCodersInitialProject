c4ad62cdef2043a006a291ac64687b30f56de633
Hunter <hunterdhodge@gmail.com>
2019-07-31 19:55:15 -0500

    Replace states withstore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-const states = {
+const stores = {
     'home': {
         'title': 'This is the home page!'
     },
@@ -17,23 +17,23 @@ const states = {
 
 /*
 1 add event listener for what is clicked on
-2 set state = to navigation button clicked
+2 set store = to navigation button clicked
 3 Re-render page
 */
-function render(state){
+function render(store){
     document.querySelector('#root').innerHTML = `
-${Navigation(state)}
-${Header(state)}
-${Main(state)}
-${Footer(state)}`;
+${Navigation(store)}
+${Header(store)}
+${Main(store)}
+${Footer(store)}`;
 
     const navSwitch = document.querySelectorAll('nav > ul >li:not(#dropdown)');
 
     navSwitch.forEach(function switchpage(element){
         element.addEventListener('click', function clickHandler(event){
             event.preventDefault();
-            render(states[event.target.textContent.toLowerCase()]);
+            render(stores[event.target.textContent.toLowerCase()]);
         });
     });
 }
-render(states.home);
+render(stores.home);
